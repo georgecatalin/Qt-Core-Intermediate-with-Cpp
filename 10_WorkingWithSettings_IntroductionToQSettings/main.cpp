6f8f1956d408e345313351bae9c994a5dc1fa1f7
georgecatalin <george.calin@gmail.com>
2020-05-12 15:25:36 +0300

    Introduction to QSettings II:

--- 10_WorkingWithSettings_IntroductionToQSettings/main.cpp
+++ 10_WorkingWithSettings_IntroductionToQSettings/main.cpp
@@ -10,14 +10,23 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationDomain("www.lacon.de");
     QCoreApplication::setApplicationName("Study Qt");
 
+    qInfo()<<QCoreApplication::applicationName();
+    qInfo()<<QCoreApplication::organizationName();
+    qInfo()<<QCoreApplication::organizationDomain();
+
     QSettings qsettings(QCoreApplication::organizationName(), QCoreApplication::applicationName());
 
     //set the setting
-    qsettings.setValue("Laconia",2020);
+    //qsettings.setValue("Laconia",2020);
 
     //read the setting
     qInfo()<<qsettings.value("Laconia").toString();
     qInfo()<<qsettings.value("Laconia").toInt();
+
+    QSettings qsettings2;
+    qsettings2.setValue("Technology","C++");
+
+    qInfo()<<qsettings2.value("Technology").toString();
 
     return a.exec();
 }
