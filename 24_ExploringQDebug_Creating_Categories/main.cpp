89c575149669a721ea6db4d08d8e58a9a86df4c9
George Calin <george.calin@gmail.com>
2020-05-15 17:51:48 +0300

    Creating categories.

--- 24_ExploringQDebug_Creating_Categories/main.cpp
+++ 24_ExploringQDebug_Creating_Categories/main.cpp
@@ -1,8 +1,32 @@
 #include <QCoreApplication>
+#include <QDebug>
+#include <QLoggingCategory>
+
+//Declare a logging category
+Q_DECLARE_LOGGING_CATEGORY(network);
+Q_LOGGING_CATEGORY(network,"network");
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    qInfo() << "test";
+
+    qInfo(network) << "test";
+
+    //turn it off
+    QLoggingCategory::setFilterRules("network.debug=false");
+
+    //Will not log
+    qDebug(network) << "kitty";
+
+    if(!network().isDebugEnabled()) {
+        QLoggingCategory::setFilterRules("network.debug=true");
+        qDebug(network) << "We turned it back on";
+    }
+
+    qDebug(network) << "yes!";
+
 
     return a.exec();
 }
