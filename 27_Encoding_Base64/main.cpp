6a472ef93caba869f7ee1be7d4442827b89bdbab
georgecatalin <george.calin@gmail.com>
2020-05-15 19:29:14 +0300

    Encoding to Base64

--- 27_Encoding_Base64/main.cpp
+++ 27_Encoding_Base64/main.cpp
@@ -1,8 +1,31 @@
 #include <QCoreApplication>
+#include <QDebug>
+
+QString makeData()
+{
+    QString data;
+
+    for (int i=0;i<10;i++)
+    {
+        data.append("Hello\r\n");
+    }
+
+    return data;
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    QString data=makeData();
+
+    QByteArray bytes=data.toLatin1();
+    QByteArray encoded=bytes.toBase64();
+    QByteArray decoded=QByteArray::fromBase64(encoded);
+
+    qInfo()<<"Encoded.."<<encoded;
+    qInfo("**********************************");
+    qInfo()<<"Decoded.."<<decoded;
 
     return a.exec();
 }
