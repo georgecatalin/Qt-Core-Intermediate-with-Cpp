a25d3370197c806339fcd2c860e9ea54e590e1ec
George Calin <george.calin@gmail.com>
2020-05-15 17:49:05 +0300

    Installing a message handler.

--- 22_ExploringQDebug_Installing_a_message_handler/main.cpp
+++ 22_ExploringQDebug_Installing_a_message_handler/main.cpp
@@ -2,21 +2,55 @@
 #include <QDebug>
 #include <QFile>
 #include <QDateTime>
+#include <QTextStream>
 
-const QtMessageHandler QT_DEFAULT_MESSAGE_HANDLER=qInstallMessageHandler(nullptr); //add a default message handler
+const QtMessageHandler QT_DEFAULT_MESSAGE_HANDLER = qInstallMessageHandler(nullptr);
 
+void lhandler(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
 
+    QString txt;
+    switch (type) {
+    case QtInfoMsg:
+        txt = QString("Info: %1 in %2").arg(msg);
+        break;
+    case QtDebugMsg:
+        txt = QString("Debug: %1").arg(msg);
+        break;
+    case QtWarningMsg:
+        txt = QString("Warning: %1").arg(msg);
+        break;
+    case QtCriticalMsg:
+        txt = QString("Critical: %1").arg(msg);
+        break;
+    case QtFatalMsg:
+        txt = QString("Fatal: %1").arg(msg);
+        break;
+    }
 
+    QFile file("log.txt");
+    if(file.open(QIODevice::WriteOnly | QIODevice::Append)) {
+        QTextStream ts(&file);
+        ts << QDateTime::currentDateTime().toString() << " - " << txt << " file: " << context.file << " line: " << context.line << "\r\n";
+        ts.flush();
+        file.close();
+     }
+
+    (*QT_DEFAULT_MESSAGE_HANDLER)(type, context, msg);
+
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    qInfo()<<"This is a info message.";
-    qDebug()<<"This is a debug message.";
-    qWarning()<<"This is a warning message.";
-    qCritical()<<"This is a critical message.";
-    qFatal("This a fatal message and it kills the program!!");
+    qInstallMessageHandler(lhandler);
+
+    qInfo() << "This is a info message";
+    qDebug() << "This is a debug message";
+    qWarning() << "This is a warning message";
+    qCritical() << "This is a critical message";
+    qFatal("THIS IS SPARTA!!!");
 
     return a.exec();
 }
+© 2020 GitHub, Inc.
